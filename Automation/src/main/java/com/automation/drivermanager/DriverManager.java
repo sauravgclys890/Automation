5d2ade229583de299ee7cf81226f2abc88020c2c
sauravgclys890 <sauravgclys890@gmail.com>
2020-03-07 21:32:59 +0530

    Addedd drivermanager

--- Automation/src/main/java/com/automation/drivermanager/DriverManager.java
+++ Automation/src/main/java/com/automation/drivermanager/DriverManager.java
@@ -1,5 +1,32 @@
 package com.automation.drivermanager;
 
+import org.openqa.selenium.WebDriver;
+
 public abstract class DriverManager {
+	
+	protected WebDriver driver;
+	protected abstract void startService();
+	protected abstract void stopService();
+	protected abstract void createdriver();
+	
+	public void quitDriver() {
+		if(driver != null) {
+			driver.quit();
+			driver=null;
+		}
+	}
+	
+	public void maximizeWindow() {
+		driver.manage().window().maximize();
+	}
+	
+	public WebDriver getDriver() {
+		if(null == driver) {
+			startService();
+			createdriver();
+		}
+		return driver;
+	}
+	
 
 }
