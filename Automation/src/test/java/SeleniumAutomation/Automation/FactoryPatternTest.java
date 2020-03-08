6e7e64ff085c3848eacd45eedb2ac1f0d206b02b
sauravgclys890 <sauravgclys890@gmail.com>
2020-03-08 14:33:04 +0530

    added some page

--- Automation/src/test/java/SeleniumAutomation/Automation/FactoryPatternTest.java
+++ Automation/src/test/java/SeleniumAutomation/Automation/FactoryPatternTest.java
@@ -2,7 +2,10 @@ package SeleniumAutomation.Automation;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import com.automation.drivermanager.DriverManager;
 import com.automation.drivermanager.DriverManagerFactory;
